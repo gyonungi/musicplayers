47817b26ca68c1b0843480e5cea2adecabd43d46
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-10-28 17:43:27 +0700

    burger line

---
+++ src/burger__line/burger.jsx
@@ -0,0 +1,10 @@
+function burger__line(){
+    return(
+        <div class="nav__burger burger">
+        <span class="burger__line"></span>
+        <span class="<!--  -->burger__line"></span>
+        <span class="burger__line"></span>
+    </div>
+
+    );
+}
