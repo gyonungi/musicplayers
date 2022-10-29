489a099619df0ab490b579d7b98c78b377dba19f
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-10-29 17:29:57 +0700

    add menu__item

---
+++ src/Menu__Item/MenuItem.jsx
@@ -0,0 +1,11 @@
+function MenuItem(){
+    return(
+        <div class="nav__menu menu">
+                        <ul class="menu__list">
+                            <li class="menu__item"><a href="http://" class="menu__link">Главное</a></li>
+                            <li class="menu__item"><a href="http://" class="menu__link">Мой плейлист</a></li>
+                            <li class="menu__item"><a href="http://" class="menu__link">Войти</a></li>
+                        </ul>
+                    </div>
+    )
+}
