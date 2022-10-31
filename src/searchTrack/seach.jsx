8509d0a8f811a28f6da3936332606479e88c138e
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-10-31 19:38:51 +0700

    Add files via upload

---
+++ src/searchTrack/seach.jsx
@@ -0,0 +1,11 @@
+function searchTrack(){
+
+return(<div class="centerblock__search search">
+<svg class="search__svg">
+    <use xlink:href="img/icon/sprite.svg#icon-search"></use>
+</svg>    
+
+<input class="search__text" type="search" placeholder="Поиск" name="search"> </input>
+</div>
+)
+}
