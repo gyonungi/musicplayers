53cbaf6c4234cd706b9390af1951809c038e22be
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-10-31 19:39:01 +0700

    Add files via upload

---
+++ src/sidebar_item/sidebarItem.jsx
@@ -0,0 +1,31 @@
+function sidebarItem(){
+    return(
+        <div class="main__sidebar sidebar">
+        <div class="sidebar__personal">
+            <p class="sidebar__personal-name">Sergey.Ivanov</p>
+            <div class="sidebar__avatar">
+                
+            </div>
+        </div>
+        <div class="sidebar__block">
+            <div class="sidebar__list">
+                <div class="sidebar__item">
+                    <a class="sidebar__link" href="#">
+                        <img class="sidebar__img" src="img/playlist01.png" alt="day's playlist"></img>
+                    </a>
+                </div>
+                <div class="sidebar__item">
+                    <a class="sidebar__link" href="#">
+                        <img class="sidebar__img" src="img/playlist02.png" alt="day's playlist"></img>
+                    </a>
+                </div>
+                <div class="sidebar__item">
+                    <a class="sidebar__link" href="#">
+                        <img class="sidebar__img" src="img/playlist03.png" alt="day's playlist"></img>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
