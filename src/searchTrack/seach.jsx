0b3fe583920ba68b5a0ce0baf06fa2f5ccc7a987
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-11-04 00:17:00 +0700

    change xlinkHref

--- src/searchTrack/seach.jsx
+++ src/searchTrack/seach.jsx
@@ -2,7 +2,7 @@ export default function searchTrack(){
 
 return(<div class="centerblock__search search">
 <svg class="search__svg">
-    <use xlink:href="img/icon/sprite.svg#icon-search"></use>
+    <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
 </svg>    
 
 <input class="search__text" type="search" placeholder="Поиск" name="search"> </input>
