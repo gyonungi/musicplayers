b089a628eff0ea856bd1076f6955a21b574b84a0
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-11-05 18:34:19 +0700

    Add files via upload

--- src/Menu__Item/MenuItem.jsx
+++ src/Menu__Item/MenuItem.jsx
@@ -1,10 +1,10 @@
 export default function MenuItem(){
     return(
-        <div class="nav__menu menu">
-                        <ul class="menu__list">
-                            <li class="menu__item"><a href="http://" class="menu__link">Главное</a></li>
-                            <li class="menu__item"><a href="http://" class="menu__link">Мой плейлист</a></li>
-                            <li class="menu__item"><a href="http://" class="menu__link">Войти</a></li>
+        <div className="nav__menu menu">
+                        <ul className="menu__list">
+                            <li className="menu__item"><a href="http://" class="menu__link">Главное</a></li>
+                            <li className="menu__item"><a href="http://" class="menu__link">Мой плейлист</a></li>
+                            <li className="menu__item"><a href="http://" class="menu__link">Войти</a></li>
                         </ul>
                     </div>
     )
