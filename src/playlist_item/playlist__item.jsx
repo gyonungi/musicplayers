ce5a5cf71ed78a58c8b20584b599e6ffe23d99a6
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-11-05 18:35:53 +0700

    Add files via upload

--- src/playlist_item/playlist__item.jsx
+++ src/playlist_item/playlist__item.jsx
@@ -1,28 +1,28 @@
-export default function playlistItem(){
+export default function PlaylistItem(){
     return(
-        <div class="playlist__item">
-        <div class="playlist__track track">
-            <div class="track__title">
-                <div class="track__title-image">
-                    <svg class="track__title-svg" alt="music">
+        <div className="playlist__item">
+        <div className="playlist__track track">
+            <div className="track__title">
+                <div className="track__title-image">
+                    <svg className="track__title-svg" alt="music">
                         <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
                     </svg>
                 </div>
-                <div class="track__title-text">
-                    <a class="track__title-link" href="http://">Guilt <span class="track__title-span"></span></a>
+                <div className="track__title-text">
+                    <a className="track__title-link" href="http://">Guilt <span className="track__title-span"></span></a>
                 </div>
             </div>
-            <div class="track__author">
-                <a class="track__author-link" href="http://">Nero</a>
+            <div className="track__author">
+                <a className="track__author-link" href="http://">Nero</a>
             </div>
-            <div class="track__album">
-                <a class="track__album-link" href="http://">Welcome Reality</a>
+            <div className="track__album">
+                <a className="track__album-link" href="http://">Welcome Reality</a>
             </div>
-            <div class="track__time">
-                <svg class="track__time-svg" alt="time">
+            <div className="track__time">
+                <svg className="track__time-svg" alt="time">
                     <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
                 </svg>
-                <span class="track__time-text">4:44</span>
+                <span className="track__time-text">4:44</span>
             </div>
         </div>
     </div>
