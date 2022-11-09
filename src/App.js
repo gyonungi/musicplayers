d68a1b4586edd27669c2cfb708f51e6ecd867567
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-11-09 19:02:07 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import MenuItem from "./Menu__Item/MenuItem";
 import SearchTrack from "./searchTrack/seach";
 import FilterTrack from "./filter__track/filterTrack";
 import PlaylistItem from "./playlist_item/playlist__item";
-
+import PlaylistTitle from "./playlist_title/playlist_title"
+import BarPlayer from "./MusPlayer/Bar_player";
+import Skylogo from "./skylogo/sky_logo";
 
 function App() {
   return (
     <div>
-      <SearchTrack/>
-      <SidebarItem/>
+      <Skylogo/>
       <BurgerLine/>
       <MenuItem/>
+      <SearchTrack/>
       <FilterTrack/>
+      <PlaylistTitle/>
       <PlaylistItem/>
+      <SidebarItem/>
+      <BarPlayer/>
     </div>
   );
 }
