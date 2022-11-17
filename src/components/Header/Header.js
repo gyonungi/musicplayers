c536d5fbdc6a6d3e216a1cf5942e1fc8e47e1f15
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-11-17 22:03:15 +0700

    Add files via upload

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,13 @@ import Logo from "../Logo/Logo";
 import SearchInput from "../SearchInput/SearchInput"
 import React from 'react'
 import "./Header.css";
+import SearchLogo from "../searchlogo/searchlogo";
 
 const Header = ()  => {  
   return (
     <div className="header">
       <Logo />
+      <SearchLogo/>
       <SearchInput />
     </div>
   );
