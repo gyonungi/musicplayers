b63b13f94765e75559829328b84d34abcbaf1e98
gyonungi <104500843+gyonungi@users.noreply.github.com>
2022-12-01 18:58:31 +0700

    Add files via upload

--- src/components/CenterBlock/CenterBlock.js
+++ src/components/CenterBlock/CenterBlock.js
@@ -42,8 +42,12 @@ function CenterBlock() {
               <Track/>
               <Track/>
               <Track/>
+              <div className="navigationMenu">
+            <button className="navigationMenu__link navigationMenu__link_daily-playList"></button>
+            <button className="navigationMenu__link navigationMenu__link_top-100"></button>
+            <button className="navigationMenu__link navigationMenu__link_indy"></button>
             </div>    
-
+            </div>  
           </div>    
         </div>  
         
