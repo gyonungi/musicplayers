f26d845ebebb38c1964869a492f8cbf3bf9238ba
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-01-03 20:35:17 +0700

    Add files via upload

---
+++ src/components/Reag/Reag.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const RegPass = styled.input`
+position: absolute;
+width: 278.5px;
+height: 0px;
+left: 40.5px;
+top: 208px;
+
+border: 1px solid #D0CECE;
+`;
+
+export const RegLog = styled.input`
+position: absolute;
+width: 278.5px;
+height: 0px;
+left: 40.5px;
+top: 208px;
+
+border: 1px solid #D0CECE;
+`;
+
+export const RegLogButton = styled.button`
+position: absolute;
+width: 278px;
+height: 52px;
+left: 41px;
+top: 268px;
+
+background: #580EA2;
+border-radius: 6px;
+
+width: 51px;
+height: 24px;
+left: 155px;
+top: 280px;
+font-family: 'StratosSkyeng';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 24px;
+color: #FFFFFF;
+`;
+
+export const RegButton = styled.button`
+position: absolute;
+width: 278px;
+height: 52px;
+left: 41px;
+top: 340px;
+border: 1px solid #D0CECE;
+border-radius: 6px;
+
+width: 177px;
+height: 24px;
+left: 91px;
+top: 352px;
+font-family: 'StratosSkyeng';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 24px;
+letter-spacing: -0.003em;
+color: #000000;
+`;
