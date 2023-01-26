ea23a9fb7442b684755e4e4d57ecd7ac8b8ffa93
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-01-26 20:52:23 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState,useContext  } from "react";
+import styled from 'styled-components'
 import './App.css';
 import Header from './components/Header/Header';
 import CenterBlock from "./components/CenterBlock/CenterBlock";
@@ -6,11 +7,30 @@ import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
 import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
 import Playerbar from "./components/Player/Player"
 import './components/Player/Player.css'
+import { StyledDivApp } from "./App.jsx";
 
 
-function App() {
+
+
+function App(props) {
+  const StyledDivApp = styled.div`
+  width: 100%;
+  min-height: 100%;
+  overflow: scroll;
+  max-width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  position: relative;
+  background-color: ${props => props.theme.dark};
+  `;
+  const theme = {
+    dark: 'rgba(0, 0, 0, 0.85)',
+  };
+
+
   return (
-    <div className="App">  
+  
+    <StyledDivApp theme={theme}>  
       <Header/>
       <section className="main">          
         <BurgerMenu/>
@@ -18,9 +38,8 @@ function App() {
         <NavigationMenu/>        
       </section>
       <Playerbar/>
-    </div>
+    </StyledDivApp>
   );
 }
-
-
 export default App;
+
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledDivApp = styled.div`
+width: 100%;
+min-height: 100%;
+overflow: scroll;
+max-width: 100%;
+height: 100vh;
+margin: 0 auto;
+position: relative;
+background-color: rgba(0, 0, 0, 0.85);
+`;
