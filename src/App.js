9ceae95b887b02409c9d0d28aec649d55689931e
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-02-18 19:45:28 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from "react";
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 import CenterBlock from "./components/CenterBlock/CenterBlock";
 import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
 import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
-import Playerbar from "./components/Player/Player"
-import './components/Player/Player.css'
+import Playerbar from "./components/Player/Player";
+import "./components/Player/Player.css";
 import { StyledDivApp } from "./App.jsx";
-import {TodoList } from './components/todo-list';
-import { AddTodo } from "./components/add-todo";
-function App() {
-
+const App = () => {
 
   return (
     <StyledDivApp>
-       <AddTodo />
-        <TodoList />
-      <Header/>
-      <section className="main">          
-        <BurgerMenu/>
-        <CenterBlock/>
-        <NavigationMenu/>        
+     <Header />
+      <section className="main">
+        <BurgerMenu />
+        <CenterBlock />
+        <NavigationMenu />
       </section>
-      <Playerbar/>
+      <Playerbar /> 
     </StyledDivApp>
-
   );
-}
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Contex from "./contex/Contex.jsx"
-import { Provider } from "react-redux";
-import { store } from "./Store/store";
+
 /* 
 const burgeritem = document.querySelector(".menu__list");
 const burgerMenu = document.querySelector(".nav__burger");
@@ -31,9 +30,7 @@ function burgerHandler () {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
     <Contex/>
-    </Provider>
   </React.StrictMode>
 );
 
