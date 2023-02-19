38b6198ea4714811d8f547bd2507d8ce9a7bc9e9
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-02-19 21:09:38 +0700

    Delete src/protected-route directory

--- src/protected-route/protectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-
-import {Navigate, Outlet} from 'react-router-dom';
-
-
-
-export const ProtectedRoute = ({ isAllowed, redirectTo = '/' }) =>
-  isAllowed ? <Outlet /> : <Navigate to={redirectTo} />;
-
-export default ProtectedRoute
