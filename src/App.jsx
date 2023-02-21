e27296a8f6b43d608d4950f9518bb91d934e8c34
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-02-21 16:23:22 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import CenterBlock from "./components/CenterBlock/CenterBlock";
+import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
+import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
+import Playerbar from "./components/Player/Player";
+import "./components/Player/Player.css";
+import { StyledDivApp } from "./App.styled";
+const App = () => {
+
+  return (
+    <StyledDivApp>
+     <Header />
+      <section className="main">
+        <BurgerMenu />
+        <CenterBlock />
+        <NavigationMenu />
+      </section>
+      <Playerbar /> 
+    </StyledDivApp>
+  );
+};
+export default App;
