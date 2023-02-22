b4884fa160b44cd7b552ffb052a48299808d72d4
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-02-22 16:45:54 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from "react-redux";
 import { AppRoutes } from './Routes.jsx';
-
+import {store} from './store/store'
 /* 
 const burgeritem = document.querySelector(".menu__list");
 const burgerMenu = document.querySelector(".nav__burger");
@@ -30,9 +31,13 @@ function burgerHandler () {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
+     <Provider store={store}>
   <React.StrictMode>
     <AppRoutes/>
   </React.StrictMode>
+  </Provider>
+  </BrowserRouter>   
 );
 
 
