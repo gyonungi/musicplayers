97ea3ab9545555e20a0918d29fe319c09cd222b9
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-02-23 23:50:39 +0700

    Add files via upload

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,100 +1,59 @@
-import styled from "styled-components";
-import TrackSVG from "../../images/icon/note.svg" 
+import React from 'react'
 
-export const TrackDiv = styled.div`
-text-decoration: none;
-display: flex;
-align-items: center;
-`;
+import { TrackDiv,TrackTitle,TrackTitleImg,TrackTitleSVG,TrackTitleText,TrackTitleLink,TrackTitleSpan,TrackAuthor,TrackAuthorLink,TrackAlbum,TrackAlbumLink,TrackTimeSVG,TrackTimeText } from './Track.styled'
 
-export const TrackTitle = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: 447px;
-`;
+const PlaylistItem = ({ id, track, artist, album, time, trackId, favTracks }) => {
+  const handleTrackId = () => {
+    trackId(id);
+  };
 
-export const TrackTitleImg = styled.div`
-width: 51px;
-height: 51px;
-padding: 16px;
-background: #313131;
-display: flex;
-align-items: center;
-justify-content: center;
-margin-right: 17px;
-`;
 
-export const TrackTitleSVG = styled.svg`
-   width: 35px;
-    height: 20px;
-    fill: transparent;
-    stroke: #4E4E4E;
-    background-image: url(${TrackSVG});
-    background-repeat: no-repeat;
-`;
+  function fancyTimeFormat(duration) {
+    const hrs = ~~(duration / 3600);
+    const mins = ~~((duration % 3600) / 60);
+    const secs = ~~duration % 60;
+    let timetrack = '';
 
-export const TrackTitleText = styled.div`
+    if (hrs > 0) {
+      timetrack += '' + hrs + ':' + (mins < 10 ? '0' : '');
+    }
+    timetrack += '' + mins + ':' + (secs < 10 ? '0' : '');
+    timetrack += '' + secs;
+    return timetrack;
+  }
+  return (
+      <TrackDiv>
+        <TrackTitle onClick={handleTrackId}>
+          <TrackTitleImg>
+            <TrackTitleSVG >
+              <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+            </TrackTitleSVG>
+          </TrackTitleImg>
+          <TrackTitleText>
+            <TrackTitleLink  href="http://">
+              Mode <TrackTitleSpan>{track}</TrackTitleSpan>
+            </TrackTitleLink>
+          </TrackTitleText>
+        </TrackTitle>
+        <TrackAuthor>
+          <TrackAuthorLink href="http://">
+          {artist}
+          </TrackAuthorLink>
+        </TrackAuthor>
+        <TrackAlbum>
+          <TrackAlbumLink href="http://">
+                {album}    
+          </TrackAlbumLink>
+        </TrackAlbum>
+        <div className="track__time">
+          <TrackTimeSVG alt="time">
+            <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+          </TrackTimeSVG>
+          <TrackTimeText >{fancyTimeFormat(time)}</TrackTimeText>
+        </div>
+      </TrackDiv>
    
-`;
+  )
+}
 
-export const TrackTitleLink = styled.a`
-   font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 24px;
-    color: #FFFFFF;
-`;
-
-export const TrackTitleSpan = styled.span`
-   font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 24px;
-    color: #4E4E4E;
-`;
-
-export const TrackAuthor = styled.div`
-    width: 321px;
-    display: flex;
-    justify-content: flex-start;
-`;
-
-export const TrackAuthorLink = styled.a`
-    font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 24px;
-    color: #FFFFFF;
-    text-align: left;
-`;
-
-export const TrackAlbum = styled.div`
-   width: 245px;
-`;
-
-export const TrackAlbumLink = styled.a`
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 24px;
-color: #696969;
-`;
-
-
-export const TrackTimeSVG = styled.svg`
- width: 14px;
-    height: 12px;
-    margin-right: 17px;
-    fill: transparent;
-    stroke: #696969;
-`;
-
-export const TrackTimeText = styled.span`
-   font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 24px;
-    text-align: right;
-    color: #696969;
-`;
+export default PlaylistItem
