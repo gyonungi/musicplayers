c4290fd033f66d1aba5e127358dc630924f78b2b
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-03-06 19:51:39 +0700

    Add files via upload

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,15 +1,102 @@
+import React from 'react';
+import search from '../../images/icon/search.svg'
+import styles from './Search.module.scss'
+import { useState, useEffect, useRef } from 'react';
+import { useGetAllTracksQuery } from '../../backend/tracks'
+const Search = () => {
+  const { data: allTracks = [], error, isLoading } = useGetAllTracksQuery();
+  const [searchInput, setSearchInput] = useState('');
+  const [searchedTracks, setSearchedTracks] = useState(undefined);
+  const [searchHidden, setSearchHidden] = useState(true);
+  const [chosenTrack, setChosenTrack] = useState(undefined);
+  const searchBoxRef = useRef();
 
-import React from 'react'
-//import { Route, Switch} from 'react-router-dom'
+  useEffect(() => {
+    let handler = (e) => {
+      if (!searchBoxRef.current.contains(e.target)) {
+        setSearchHidden(true);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => document.removeEventListener('mousedown', handler);
+  });
 
-import { HeaderInput } from './SearchInput.styled';
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
 
-const SearchInput = ()  => {
-  
-  
+  const getTrack = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.textContent);
+    setChosenTrack(e.target.textContent);
+    setSearchHidden(true);
+  };
+
+  useEffect(() => {
+    if (searchedTracks === undefined) {
+      return;
+    }
+    if (searchedTracks.length === 0) {
+      setSearchHidden(true);
+    }
+  }, [searchedTracks]);
+
+  useEffect(() => {
+    if (chosenTrack) {
+      const target = allTracks.find((track) => track.name === chosenTrack);
+      console.log(target.id);
+    }
+  }, [chosenTrack]);
+
+
+
+  useEffect(() => {
+    if (allTracks) {
+      setSearchedTracks(
+        allTracks.filter((track) => {
+          return track.name.toLowerCase().match(searchInput.toLowerCase());
+        })
+      );
+    }
+    if (searchInput.length !== 0) {
+      setSearchHidden(false);
+    } else {
+      setSearchHidden(true);
+    }
+  }, [searchInput]);
+
   return (
-    <HeaderInput  placeholder='Поиск'></HeaderInput>
+    <div className={styles.centerblock__search}>
+      <input
+        className={styles.search__text}
+        type="search"
+        placeholder="Поиск"
+        name="search"
+        onChange={handleChange}
+        value={searchInput}
+      />
+      <div
+        ref={searchBoxRef}
+        className={
+          searchHidden === true
+            ? `${styles.search__result_box} ${styles.search_hidden}`
+            : `${styles.search__result_box}`
+        }
+      >
+        {searchedTracks
+          ? searchedTracks.map((track,index) => (
+              <div
+                onClick={getTrack}
+                className={styles.search__result_item}
+                key={index}
+              >
+                {track.name}
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
   );
 };
-
-export default SearchInput;
+export default Search
